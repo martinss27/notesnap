4b32c04a8b66b8f817e9a38ccba3e242c9d95f49
martinss27 <leoalmeidamartins@gmail.com>
2025-07-21 15:50:36 -0300

    creating models for user

--- models/user.py
+++ models/user.py
@@ -1,2 +1,10 @@
 from sqlalchemy import column, Integer, String
 from database import Base
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = column(Integer, primary_key=True, index=True)
+    email = column(String, unique=True, index=True, nullable=False)
+    hashed_password = column(String, nullable=False)
